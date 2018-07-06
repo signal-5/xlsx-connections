9d16e49047fe11881f9e41bac1561f54da5526e7
signal-5 <github@sigtrap.org>
2018-07-06 16:40:40 +0200

    Create xlsx-connections.pl

---
+++ xlsx-connections.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+die "Need site\n" if @ARGV!=1;
+
+$nextPage=1;
+$totalResults=2;
+$site=$ARGV[0];
+# Get your cx here: https://cse.google.com/all
+$cx=YOUR_CX;
+# Get your key here: https://developers.google.com/custom-search/json-api/v1/introduction#identify_your_application_to_google_with_api_key 
+$key=YOUR_KEY;
+
+while($nextPage<92 and $totalResults>$nextPage) {
+  $search=`wget -qO- "https://www.googleapis.com/customsearch/v1?key=$key&q=ext%3Axlsx+site%3A$site&cx=$cx&filter=0&start=$nextPage"`;
+  (@lines)=$search=~/\"link\": \"([^\"]+)\",/gs;
+  ($nextPage)=$search=~/\"nextPage\": .*?\"startIndex\": (\d+),/s;
+  ($totalResults)=$search=~/\"totalResults\": \"(\d+)\",/s;
+  # Add for debugging:
+  # print "$nextPage, $totalResults, @lines\n";
+
+  foreach $URL (@lines) {
+    $result=`wget -qO- $URL | bsdtar -tf- | grep connections && echo $URL`;
+    # Replace for debugging:
+    # $result=`wget -qO- $URL | bsdtar -tf- | grep connections ; echo $URL`;
+    print $result;
+  }
+
+}
